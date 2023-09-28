cf66f098e75157452ff7f96964c5216e8f7c780d
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2023-09-28 07:55:24 +0100

    Added setup for Dapp dev

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,2 +1,6 @@
 export const axiosInstance = {};
-export const ethers = {};
+export const PRIVATE_KEY = {
+    LOCALNET: process.env.LOCALNET_WALLET_PRIVATE_KEY,
+    TESTNET: process.env.TESTNET_DEV_WALLET_PRIVATE_KEY,
+    MAINNET: process.env.MAINNET_DEV_WALLET_PRIVATE_KEY,
+};
